1ae4bf50386b20c457ed3ae8e0ac1c693b9343a8
worksofdeep <worksofdeep@gmail.com>
2024-01-29 13:04:07 +0530

    Blank Project

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import React from "react";
 
 const App = () => {
-
-  const name = "John";
-  const isRegistered = false;
-
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    // setCount(100);
-    alert('You have changed count');
-  }, [count]) // Array of variables to monitor
-
-  return (
-    <div className="App">
-      <button onClick={() => { setCount((prevCount) => prevCount - 1) }}>-</button>
-      <h3>{count}</h3>
-      <button onClick={() => { setCount((prevCount) => prevCount + 1) }}>+</button>
-    </div>
-  );
-}
-
-// Person Component
-const Person = (props) => {
-
-  return (
-    <>
-      <p>Name: {props['name'] ? props['name'] : 'Unknown'}</p>
-      <p>Last Name: {props['lastName']}</p>
-      <p>Age: {props['age']}</p>
-    </>
-
-  );
+    return (
+        <h1>App</h1>
+    )
 }
 
 export default App;
